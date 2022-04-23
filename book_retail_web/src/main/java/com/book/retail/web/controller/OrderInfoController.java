c77c1162021f13f694ec234178577d8b7fbd48ec
liufuwei <1021901278@qq.com>
2022-04-23 21:11:42 +0800

    update info

--- book_retail_web/src/main/java/com/book/retail/web/controller/OrderInfoController.java
+++ book_retail_web/src/main/java/com/book/retail/web/controller/OrderInfoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/order")
-public class OrderInfoController extends BaseController {
+public class OrderInfoController{
 
     @Autowired
     OrderInfoService orderInfoService;
